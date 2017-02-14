b659de3e03999ff9c8ef6ebb2297543ab56a431e
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-02-14 02:49:03 -0800

    add shim cmsys/Encoding.h

---
+++ 3rdparty/cmake-3.7.2/Source/cmsys/Encoding.h
@@ -0,0 +1,12 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file Copyright.txt or https://cmake.org/licensing for details.  */
+
+#pragma once
+
+#include <stdlib.h>
+
+static wchar_t* cmsysEncoding_DupToWide(const char* str)
+{
+  abort();
+  return NULL;
+}
