cd787eb3c22fa90b30a2626c993bc9080350b1ba
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-02-14 01:59:29 -0800

    parser.cpp: rewrite to use cmListFileLexer
    This breaks the IndentArgumentsFormatter, because I realized that
    arguments aren't always a linear collection. Parentheses will need
    to be handled specially.

--- parser.cpp
+++ parser.cpp
@@ -4,245 +4,155 @@
 
 #include <cctype>
 
+#include "cmListFileLexer.h"
 #include "parser.h"
 
-struct CMakeParser {
-	CMakeParser(const std::string &content, std::vector<Span> &spans);
+parseexception::parseexception(const std::string &message) : std::runtime_error{message} {
+}
 
-	void skip_ws();
-	Command parse_command_invocation();
-	Span parse_identifier();
-	void parse_lparen();
-	void parse_rparen();
-	void parse_dollar_sign_expression();
-	Span parse_quoted_argument();
-	Span parse_unquoted_argument();
+struct Lexer {
 
-	void expect(bool condition, const std::string &description);
+	Lexer(const std::string &data) {
+		lexer = cmListFileLexer_New();
+		if (!lexer) {
+			throw std::runtime_error("couldn't allocate cmListFileLexer");
+		}
 
-	const std::string &content;
-	size_t p{0};
-	std::vector<Span> &spans;
+		(void)cmListFileLexer_SetString(lexer, data.c_str());
+		// TODO: handle result
+
+		advance();
+	}
+	~Lexer() {
+		cmListFileLexer_Delete(lexer);
+	}
+
+	void advance() {
+		token = cmListFileLexer_Scan(lexer);
+	}
+
+	cmListFileLexer_Token *token;
+
+  private:
+	cmListFileLexer *lexer;
 };
 
-static bool isIdentifierStart(char c) {
-	return std::isalpha(c) || c == '_';
-}
-
-static bool isIdentifier(char c) {
-	return std::isalnum(c) || c == '_';
-}
-
-void CMakeParser::expect(bool condition, const std::string &description) {
-	if (!condition) {
-		throw parseexception("FAILURE: Expected " + description + " at " + std::to_string(p) +
-		                     ", got:\n" + content.substr(p, p + 50));
-	}
-}
-
-CMakeParser::CMakeParser(const std::string &content_, std::vector<Span> &spans_)
-    : content(content_), spans(spans_) {
-}
-
-Span CMakeParser::parse_identifier() {
-	expect(isIdentifierStart(content[p]), "identifier start");
-	size_t ident_start = p;
-	p++;
-	while (isIdentifier(content[p])) {
-		p++;
-	}
-	return {SpanType::Identifier, content.substr(ident_start, p - ident_start)};
-}
-
-void CMakeParser::skip_ws() {
+void skip_whitespace(std::vector<Span> &spans, Lexer &lexer) {
 	while (true) {
-		if (content[p] == '\n') {
-			spans.emplace_back(SpanType::Newline, content.substr(p, 1));
-			p++;
-			if (!std::isspace(content[p])) {
-				// HACK: Make sure newlines are always followed by whitespace, this makes
-				// transformations easier.
+		if (!lexer.token) {
+			break;
+		} else if (cmListFileLexer_Token_Space == lexer.token->type) {
+			spans.emplace_back(SpanType::Space, lexer.token->text);
+			lexer.advance();
+
+		} else if (cmListFileLexer_Token_Newline == lexer.token->type) {
+			spans.emplace_back(SpanType::Newline, lexer.token->text);
+			lexer.advance();
+			// HACK: Make sure newlines are always followed by whitespace, this makes
+			// transformations easier.
+			if (!lexer.token || lexer.token->type != cmListFileLexer_Token_Space) {
 				spans.emplace_back(SpanType::Space, "");
 			}
-			continue;
-		}
-		if (std::isspace(content[p])) {
-			size_t space_start = p;
-			while (std::isspace(content[p]) && content[p] != '\n') {
-				p++;
-			}
-			spans.emplace_back(SpanType::Space, content.substr(space_start, p - space_start));
-			continue;
-		}
-		if (content[p] == '#') {
-			size_t comment_start = p;
-			while (content[p] != '\n') {
-				p++;
-			}
-			spans.emplace_back(SpanType::Comment, content.substr(comment_start, p - comment_start));
-			continue;
-		}
-		break;
-	}
-}
 
-Command CMakeParser::parse_command_invocation() {
-	// HACK: Make sure command identifiers are always preceded by whitespace
-	if (spans.size() == 0 || spans.back().type != SpanType::Space) {
-		spans.emplace_back(SpanType::Space, "");
-	}
+		} else if (cmListFileLexer_Token_Comment == lexer.token->type) {
+			spans.emplace_back(SpanType::Comment, lexer.token->text);
+			lexer.advance();
 
-	spans.push_back(parse_identifier());
-	const size_t identifier = spans.size() - 1;
-	skip_ws();
-	parse_lparen();
-	skip_ws();
-	std::vector<size_t> arguments;
-	while (true) {
-		if (content[p] == ')') {
-			parse_rparen();
-			break;
-		} else if (content[p] == '"') {
-			spans.push_back(parse_quoted_argument());
-			arguments.push_back(spans.size() - 1);
 		} else {
-			spans.push_back(parse_unquoted_argument());
-			arguments.push_back(spans.size() - 1);
+			break;
 		}
-		skip_ws();
 	}
-
-	return {identifier, arguments};
 }
 
-void CMakeParser::parse_lparen() {
-	expect(content[p] == '(', "left paren");
-	spans.emplace_back(SpanType::Lparen, content.substr(p, 1));
-	p++;
+std::string tokentostring(const cmListFileLexer_Token *token) {
+	if (!token)
+		return "end-of-input";
+	return cmListFileLexer_GetTypeAsString(nullptr, token->type);
 }
 
-void CMakeParser::parse_rparen() {
-	expect(content[p] == ')', "right paren");
-	if (spans.back().type != SpanType::Space) {
-		// HACK: Make sure right-parens are always preceded by whitespace, this makes
-		// transformations easier.
-		spans.emplace_back(SpanType::Space, "");
+void expecttokentype(const std::string &description, const cmListFileLexer_Token *token,
+                     const std::vector<cmListFileLexer_Type> &wanted_types) {
+	for (auto w : wanted_types) {
+		if (token && token->type == w)
+			return;
 	}
-	spans.emplace_back(SpanType::Rparen, content.substr(p, 1));
-	p++;
+	throw parseexception("expected " + description + ", got " + tokentostring(token) + ": '" +
+	                     (token ? std::string{token->text} : "") + "'");
 }
 
-void CMakeParser::parse_dollar_sign_expression() {
-	expect(content[p] == '$', "dollar-sign");
-	p++;
+void parse_argument(std::vector<Span> &spans, Lexer &lexer) {
+	if (lexer.token && lexer.token->type == cmListFileLexer_Token_ParenLeft) {
+		spans.emplace_back(SpanType::Lparen, lexer.token->text);
+		lexer.advance();
 
-	char endbracket = 0;
-	if (content[p] == '{') {
-		p++;
-		endbracket = '}';
-	} else if (content[p] == '(') {
-		p++;
-		endbracket = ')';
-	} else if (content[p] == '<') {
-		p++;
-		endbracket = '>';
+		while (true) {
+			skip_whitespace(spans, lexer);
+			if (lexer.token && lexer.token->type == cmListFileLexer_Token_ParenRight) {
+				if (spans.back().type != SpanType::Space) {
+					// HACK: Make sure right-parens are always preceded by whitespace, this makes
+					// transformations easier.
+					spans.emplace_back(SpanType::Space, "");
+				}
+				spans.emplace_back(SpanType::Rparen, lexer.token->text);
+				lexer.advance();
+				break;
+			}
+			parse_argument(spans, lexer);
+		}
+	} else if (lexer.token && lexer.token->type == cmListFileLexer_Token_Identifier) {
+		spans.emplace_back(SpanType::Unquoted, lexer.token->text);
+		lexer.advance();
+	} else if (lexer.token && lexer.token->type == cmListFileLexer_Token_ArgumentUnquoted) {
+		spans.emplace_back(SpanType::Unquoted, lexer.token->text);
+		lexer.advance();
+	} else if (lexer.token && lexer.token->type == cmListFileLexer_Token_ArgumentQuoted) {
+		spans.emplace_back(SpanType::Quoted, "\"" + std::string{lexer.token->text} + "\"");
+		lexer.advance();
 	} else {
-		expect(false, "'{', '(', or '<'");
+		expecttokentype("argument or rparen", lexer.token, {});
 	}
-
-	while (true) {
-		// HACK: this is more lenient than the real CMake parser
-		if (content[p] == '$') {
-			parse_dollar_sign_expression();
-			continue;
-		} else if (content[p] == endbracket) {
-			p++;
-			break;
-		}
-		p++;
-	}
-}
-
-Span CMakeParser::parse_quoted_argument() {
-	expect(content[p] == '"', "double-quote");
-	size_t quoted_argument_start = p;
-	p++;
-	while (true) {
-		if (content[p] == '\\') {
-			p++;
-			p++;
-		} else if (content[p] == '"') {
-			p++;
-			break;
-		} else {
-			p++;
-		}
-	}
-	return {SpanType::Quoted, content.substr(quoted_argument_start, p - quoted_argument_start)};
-}
-
-static bool is_unquoted_element(const char c) {
-	// TODO: instead of std::isspace, also need to skip anything CMake thinks is
-	// whitespace (like comments)
-	return !(std::isspace(c) || c == '\\' || c == '$' || c == '"' || c == '(' || c == ')');
-}
-
-Span CMakeParser::parse_unquoted_argument() {
-	size_t unquoted_argument_start = p;
-	size_t parens_level = 0;
-	while (true) {
-		if (content[p] == '\\') {
-			p += 2;
-			continue;
-		}
-		if (content[p] == '$') {
-			parse_dollar_sign_expression();
-			continue;
-		}
-		if (content[p] == '"') {
-			parse_quoted_argument();
-			continue;
-		}
-		if (content[p] == '(') {
-			parens_level++;
-			p++;
-			continue;
-		}
-		if (content[p] == ')' && parens_level > 0) {
-			parens_level--;
-			p++;
-			continue;
-		}
-
-		if (is_unquoted_element(content[p])) {
-			while (is_unquoted_element(content[p])) {
-				p++;
-			}
-			continue;
-		}
-		break;
-	}
-	if (unquoted_argument_start == p) {
-		throw parseexception("expected unquoted argument");
-	}
-	return {SpanType::Unquoted,
-	        content.substr(unquoted_argument_start, p - unquoted_argument_start)};
-}
-
-parseexception::parseexception(const std::string &message) : std::runtime_error{message} {
 }
 
 std::pair<std::vector<Span>, std::vector<Command>> parse(const std::string &content) {
 	std::vector<Command> commands;
 	std::vector<Span> spans;
-	CMakeParser parser{content, spans};
+
+	Lexer lexer{content};
 
 	while (true) {
-		parser.skip_ws();
-		if (parser.p >= content.size()) {
+		skip_whitespace(spans, lexer);
+		if (!lexer.token) {
 			break;
 		}
-		commands.push_back(parser.parse_command_invocation());
+
+		// TODO: investigate replacing direct use of tokens with a shim that:
+		// - treats EOI as another token type
+		// - doesn't use such frickin' long enum names
+		expecttokentype("whitespace or identifier", lexer.token,
+		                {cmListFileLexer_Token_Identifier});
+		// HACK: Make sure command identifiers are always preceded by whitespace
+		if (spans.size() == 0 || spans.back().type != SpanType::Space) {
+			spans.emplace_back(SpanType::Space, "");
+		}
+		spans.emplace_back(SpanType::Identifier, lexer.token->text);
+		commands.push_back({spans.size() - 1, {}});
+		lexer.advance();
+
+		skip_whitespace(spans, lexer);
+
+		expecttokentype("whitespace or left paren", lexer.token, {cmListFileLexer_Token_ParenLeft});
+		spans.emplace_back(SpanType::Lparen, lexer.token->text);
+		lexer.advance();
+
+		while (true) {
+			skip_whitespace(spans, lexer);
+			if (lexer.token && lexer.token->type == cmListFileLexer_Token_ParenRight) {
+				spans.emplace_back(SpanType::Rparen, lexer.token->text);
+				lexer.advance();
+				break;
+			}
+			parse_argument(spans, lexer);
+		}
 	}
 
 	return std::make_pair(std::move(spans), std::move(commands));
