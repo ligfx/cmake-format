479cd8cf193ada79382d75e57b183f3fb94ecbc4
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-02-13 17:36:31 -0800

    add (currently failing) test_parsing.cpp

---
+++ test_parsing.cpp
@@ -0,0 +1,46 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file Copyright.txt or https://opensource.org/licenses/BSD-3-Clause for
+   details.  */
+
+#include <catch.hpp>
+
+#include "parser.h"
+
+TEST_CASE("Parses CMake code", "[parsing]") {
+	std::ignore = parse(R"(
+
+cmake_command_without_arguments()
+_underscore_cmake_command()
+UPPERCASE_CMAKE_COMMAND()
+c3e_c5d_w2h_n5s()
+simple_cmake_command(
+	()
+	simple_argument
+	UPPERCASE_ARGUMENT
+	_argument_starting_with_underscore
+	-argument-with-dashes
+	a6t_w2h_n5s
+	1234567890
+	argument;with;semicolons
+	argument/with/slashes
+	argument\\with\\backslashes
+	argument_with_\(parentheses\)
+	"quoted argument"
+	bare_argument_with_"quoted argument"
+	# comment
+	${bare_variable_reference}
+	${make_style_base_variable_reference}
+	${variable_reference_${embedded_variable_reference}}
+	$<$<generator_expression>:value can be anything! ${variable_reference}>
+)
+
+)");
+}
+
+TEST_CASE("Doesn't hang on unbalanced parentheses", "[parsing]") {
+	std::ignore = parse(R"(
+
+	command(
+
+	)");
+}
