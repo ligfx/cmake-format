3e2da00fff718c4a35d11c18028bb1eecaf7b2a8
Michael Maltese <michaeljosephmaltese@gmail.com>
2017-02-17 19:11:39 -0800

    clean up transform.h

--- transform.h
+++ transform.h
@@ -10,17 +10,12 @@
 #include "helpers.h"
 #include "parser.h"
 
-void transform_argument_bin_pack(std::vector<Command> &commands, std::vector<Span> &spans,
-    size_t column_limit, const std::string &argument_indent_string);
-void transform_argument_heuristic(std::vector<Command> &commands, std::vector<Span> &spans,
-    size_t column_limit, const std::string &argument_indent_string);
-void transform_argument_per_line(std::vector<Command> &commands, std::vector<Span> &spans,
-    const std::string &argument_indent_string);
-void transform_command_case(
-    std::vector<Command> &commands, std::vector<Span> &spans, LetterCase letter_case);
-void transform_indent(
-    std::vector<Command> &commands, std::vector<Span> &spans, const std::string &indent_string);
-void transform_indent_rparen(std::vector<Command> &commands, std::vector<Span> &spans,
-    const std::string &rparen_indent_string);
-void transform_squash_empty_lines(
-    std::vector<Command> &commands, std::vector<Span> &spans, size_t max_empty_lines);
+void transform_argument_bin_pack(
+    std::vector<Command> &, std::vector<Span> &, size_t, const std::string &);
+void transform_argument_heuristic(
+    std::vector<Command> &, std::vector<Span> &, size_t, const std::string &);
+void transform_argument_per_line(std::vector<Command> &, std::vector<Span> &, const std::string &);
+void transform_command_case(std::vector<Command> &, std::vector<Span> &, LetterCase);
+void transform_indent(std::vector<Command> &, std::vector<Span> &, const std::string &);
+void transform_indent_rparen(std::vector<Command> &, std::vector<Span> &, const std::string &);
+void transform_squash_empty_lines(std::vector<Command> &, std::vector<Span> &, size_t);
